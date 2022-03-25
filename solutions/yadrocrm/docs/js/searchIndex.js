5f8d3a4fbca8f269308c3c0b25b68aa4deb6d430
Andrey Manzadey <andrey.manzadey@gmail.com>
2022-03-25 06:36:50 +0300

    yadrocrm docs create

---
+++ solutions/yadrocrm/docs/js/searchIndex.js
@@ -0,0 +1,34 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\YadroCRM\\YadroCRM",
+            "name": "YadroCRM",
+            "summary": "",
+            "url": "classes/YadroCRM-YadroCRM.html"
+        },                {
+            "fqsen": "\\YadroCRM\\YadroCRM\u003A\u003Apush\u0028\u0029",
+            "name": "push",
+            "summary": "\u041E\u0442\u043F\u0440\u0430\u0432\u043B\u044F\u0435\u0442\u0020\u0434\u0430\u043D\u043D\u044B\u0435\u0020POST\u0020\u043D\u0430\u0020\u0441\u0435\u0440\u0432\u0435\u0440\u0020yadroCRM",
+            "url": "classes/YadroCRM-YadroCRM.html#method_push"
+        },                {
+            "fqsen": "\\YadroCRM\\YadroCRM\u003A\u003Alog\u0028\u0029",
+            "name": "log",
+            "summary": "\u041B\u043E\u0433\u0438\u0440\u043E\u0432\u0430\u043D\u0438\u0435\u0020\u043E\u0442\u0432\u0435\u0442\u0430\u0020\u0441\u0435\u0440\u0432\u0435\u0440\u0430",
+            "url": "classes/YadroCRM-YadroCRM.html#method_log"
+        },                {
+            "fqsen": "\\YadroCRM\\YadroCRM\u003A\u003AKEY",
+            "name": "KEY",
+            "summary": "\u041D\u0435\u043E\u0431\u0445\u043E\u0434\u0438\u043C\u043E\u0020\u043F\u043E\u0434\u0441\u0442\u0430\u0432\u0438\u0442\u044C\u0020\u0441\u0432\u043E\u0439\u0020\u043A\u043B\u044E\u0447",
+            "url": "classes/YadroCRM-YadroCRM.html#constant_KEY"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        },                {
+            "fqsen": "\\YadroCRM",
+            "name": "YadroCRM",
+            "summary": "",
+            "url": "namespaces/yadrocrm.html"
+        }            ]
+);
