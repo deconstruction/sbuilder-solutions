757c6b43962ed3386f10b708668db79838c1ec08
amsetere <114495719+amsetere@users.noreply.github.com>
2022-11-24 16:54:35 +0300

    Update ajax-form.js

--- solutions/ajax-form/ajax-form.js
+++ solutions/ajax-form/ajax-form.js
@@ -1,27 +1,43 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const action = '/ajax/';
+    const forms = document.querySelectorAll('[action="/ajax/"]');
+    const isDebug = document.location.search.indexOf('debug_ajax') !== -1;
 
-    console.log(new Date);
+    if (isDebug) {
+        console.info(`Load script for ajax forms. Forms finded: ${forms.length}`);
+        if (forms.length !== 0) {
+            console.info(forms);
+        }
+    }
 
-    document.querySelectorAll('[data-ajax-form]').forEach(form => {
+    
+    forms.forEach((form, i) => {
+        let formId = i;
+        if (form.getAttribute('id')) {
+            formId = form.getAttribute('id');
+        }
+
+        const eventName = `ajax_form_${formId}`;
+
+        if (isDebug) {
+            console.info(`Event name ajax form: ${eventName}`);
+        }
+
         form.addEventListener('submit', function (e) {
             e.preventDefault();
 
-            fetch(action, {
+            fetch(form.action, {
                 method: 'POST',
                 body: new FormData(form),
                 cache: 'no-cache',
             })
                 .then(r => {
                     if(r.url.indexOf('?pl') === -1) {
-                        throw Error('Форма не была отправлена! Проверьте наличие pl_plugin_ident поля в форме отправки.');
+                        throw Error('Форма не была отправлена! Проверьте наличие pl_plugin_ident поля в форме отправки или обязательные поля в технической форме.');
                     }
 
                     return r.text();
                 })
-                .then(r => {
-                    form.innerHTML = r;
-                })
+                .then(() => document.dispatchEvent(new CustomEvent(eventName, {detail:form})))
                 .catch(e => console.error(e));
         });
     });
