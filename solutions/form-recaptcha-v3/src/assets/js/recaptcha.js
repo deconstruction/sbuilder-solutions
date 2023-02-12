f828d2e17a0f1d48021f2c8e75836926d56c722e
amsetere <114495719+amsetere@users.noreply.github.com>
2023-02-12 18:28:16 +0300

    Update recaptcha.js

--- solutions/form-recaptcha-v3/src/assets/js/recaptcha.js
+++ solutions/form-recaptcha-v3/src/assets/js/recaptcha.js
@@ -4,25 +4,35 @@
  * Version: 1.1.1
  */
 
+// Listen for DOMContentLoaded and recaptcha events
 ['DOMContentLoaded', 'recaptcha'].forEach(event => {
     window.addEventListener(event, () => {
 
+        // Log that the recaptcha script has loaded
         console.log('recaptcha script loaded');
 
+        // Select all forms with data-recaptcha attribute and the meta tag with recaptcha public key
         const forms = document.querySelectorAll('[data-recaptcha]');
         const metaKey = document.querySelector('meta[name="recaptcha-public-key"]')
 
+        // If there are forms with data-recaptcha attribute and the meta tag with recaptcha public key exists
         if (forms.length > 0 && metaKey !== null) {
+            // Get the recaptcha public key from the meta tag
             const recaptchaClientKey = metaKey.getAttribute('content');
 
+            // Create a script element
             const script = document.createElement('script');
             script.onload = function () {
+                // Wait for grecaptcha to be ready
                 grecaptcha.ready(function () {
+                    // Loop through each form
                     forms.forEach(form => {
+                        // Execute the recaptcha client-side
                         grecaptcha.execute(recaptchaClientKey, {action: 'submit'}).then(function (token) {
-                            // Созадем инпут для токена и присваеваем ему полученый токен
+                            // Select the input element for the recaptcha token
                             let input = form.querySelector('[name="recaptcha_token"]');
 
+                            // If the input element doesn't exist, create it
                             if (input === null) {
                                 input = document.createElement('input');
                                 input.setAttribute("type", "hidden");
@@ -30,16 +40,17 @@
                                 form.appendChild(input);
                             }
 
+                            // Set the value of the input element to the recaptcha token
                             input.setAttribute("value", token);
 
-                            // Создаем инпуты куда помещаем информацию о текущей странице
+                            // Create an input element for the current page information
                             const page = document.createElement('input');
                             page.setAttribute("type", "hidden");
                             page.setAttribute("name", "current_page");
                             page.setAttribute("value", window.location.href);
                             form.appendChild(page);
 
-                            // Создаем инпуты куда помещаем информацию о времени создания
+                            // Create an input element for the current date information
                             const date = document.createElement('input');
                             date.setAttribute("type", "hidden");
                             date.setAttribute("name", "current_date");
@@ -50,8 +61,10 @@
                 });
             };
 
+            // Set the src of the script element to the recaptcha API
             script.src = 'https://www.google.com/recaptcha/api.js?render=' + recaptchaClientKey;
 
+            // Append the script element to the head of the document
             document.head.appendChild(script);
         }
     });
